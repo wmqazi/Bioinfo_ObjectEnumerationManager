ab1a99a6b4c43ead543be564bcfd7b1a135d5c01
Wajahat Mahmood Qazi <drwajahatmqazi@gmail.com>
2019-02-17 09:36:04 +0500

    Add files via upload

---
+++ ObjectEnumerationManager/ObjectEnumerationManager/ObjectEnumerationManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qazi.Common
+{
+    public class ObjectEnumerationManager
+    {
+    }
+}
---
+++ ObjectEnumerationManager/ObjectEnumerationManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ObjectEnumerationManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("G.C.U")]
+[assembly: AssemblyProduct("ObjectEnumerationManager")]
+[assembly: AssemblyCopyright("Copyright © G.C.U 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c52b7bd-db75-4918-840f-060389598587")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
